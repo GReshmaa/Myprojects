b759f3cb91732335fa47202a03c588d4a400be80
GReshmaa <102577848+GReshmaa@users.noreply.github.com>
2025-04-02 17:16:40 +0530

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -1,27 +1,6 @@
-
-variable "vpc" {
-  type = list(object({
-    cidr_block                           = optional(string)
-    instance_tenancy                     = optional(string)
-    ipv4_ipam_pool_id                    = optional(string)
-    ipv4_netmask_length                  = optional(string)
-    ipv6_cidr_block                      = optional(string)
-    ipv6_ipam_pool_id                    = optional(string)
-    ipv6_netmask_length                  = optional(string)
-    ipv6_cidr_block_network_border_group = optional(string)
-    enable_dns_support                   = optional(string)
-    enable_network_address_usage_metrics = optional(string)
-    enable_dns_hostnames                 = optional(string)
-    assign_generated_ipv6_cidr_block     = optional(string)
-    tags                                 = optional(map(string))
-  }))
-  default = []
+variable "route_table_id" {
+    type= string 
 }
-
-
-variable "rtassociation" {
-    type = list(object({
-       route_table_id                       = optional(string)
-       subnet_id                            = optional(string)           
-    }))
+variable "subnet_id" {
+    type = string
 }
