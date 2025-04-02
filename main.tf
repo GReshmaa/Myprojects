6ec6b16287ff8f59969c0137dae0e3ee7c4647f4
GReshmaa <102577848+GReshmaa@users.noreply.github.com>
2025-04-02 17:32:47 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
-resource "aws_route_table_association" "route-association" {
-       route_table_id = var.route_table_id
-       subnet_id      = var.subnet_id 
+resource "aws_subnet" "subnet" {
+  vpc_id                        = var.vpc_id
+  cidr_block                    = var.cidr_block
+  availability_zone             = var.availability_zone
+  map_public_ip_on_launch       = var.map_public_ip_on_launch
+   enable_lni_at_device_index = var.enable_lni_at_device_index > 0 ? var.enable_lni_at_device_index : 1
+  enable_dns64                  = var.enable_dns64
+  ipv6_cidr_block               = var.ipv6_cidr_block
+  ipv6_native                   = var.ipv6_native
+  tags                          = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,6 +1,47 @@
-variable "route_table_id" {
-    type= string 
+variable "vpc_id" {
+  description = "The ID of the VPC where the subnet will be created"
 }
-variable "subnet_id" {
-    type = string
+
+variable "cidr_block" {
+  description = "The IPv4 CIDR block for the subnet"
+ 
+}
+
+variable "availability_zone" {
+  description = "The availability zone for the subnet"
+
+}
+
+
+variable "map_public_ip_on_launch" {
+  description = "Whether instances in this subnet should be assigned a public IP"
+
+}
+
+variable "assign_ipv6_address_on_creation" {
+  description = "Assign an IPv6 address to instances in this subnet on creation"
+
+}
+
+variable "enable_dns64" {
+  description = "Enable DNS64 for the subnet"
+
+}
+
+variable "enable_lni_at_device_index" {
+  description = "Enable Local Network Interface (LNI) at device index"
+ 
+}
+variable "ipv6_cidr_block" {
+  description = "The IPv6 CIDR block for the subnet"
+
+}
+
+variable "ipv6_native" {
+  description = "Specify whether this subnet is an IPv6-only subnet"
+ 
+}
+
+variable "tags" {
+  
 }
